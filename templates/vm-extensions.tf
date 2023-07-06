54a6c46b252cbb788996ff50e342268c0fbd21a2
Marc de Droog <madedroo@microsoft.com>
2023-07-06 20:31:48 +0200

    updates

--- templates/vm-extensions.tf
+++ templates/vm-extensions.tf
@@ -86,6 +86,24 @@ resource "azurerm_virtual_machine_extension" "install-iis-onprem-vm" {
 SETTINGS
 }
 ##########################################################
+## Install IIS role on onprem2
+##########################################################
+resource "azurerm_virtual_machine_extension" "install-iis-onprem2-vm" {
+    
+  name                 = "install-iis-spoke-onprem2-vm"
+  virtual_machine_id   = azurerm_windows_virtual_machine.onprem2-vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+   settings = <<SETTINGS
+    {
+        "commandToExecute":"powershell -ExecutionPolicy Unrestricted Add-WindowsFeature Web-Server; powershell -ExecutionPolicy Unrestricted Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"
+    }
+SETTINGS
+}
+/*
+##########################################################
 ## Install ADDC role on spoke-addc-vm
 ##########################################################
 resource "azurerm_virtual_machine_extension" "install-spoke-addc-vm" {
@@ -119,7 +137,7 @@ resource "azurerm_virtual_machine_extension" "enable-routing-nva-iptables-vm" {
         "script": "c3VkbyBjaG1vZCA3NzcgL2V0Yy9zeXNjdGwuY29uZgplY2hvICJuZXQuaXB2NC5pcF9mb3J3YXJkID0gMSIgPiAvZXRjL3N5c2N0bC5jb25mCnN1ZG8gc3lzY3RsIC1wIC9ldGMvc3lzY3RsLmNvbmYKc3VkbyBpcHRhYmxlcyAtdCBuYXQgLUEgUE9TVFJPVVRJTkcgLWQgMTAuMC4wLjAvOCAtaiBBQ0NFUFQKc3VkbyBpcHRhYmxlcyAtdCBuYXQgLUEgUE9TVFJPVVRJTkcgLWQgMTcyLjE2LjAuMC8xMiAtaiBBQ0NFUFQKc3VkbyBpcHRhYmxlcyAtdCBuYXQgLUEgUE9TVFJPVVRJTkcgLWQgMTkyLjE2OC4wLjAvMTYgLWogQUNDRVBUCnN1ZG8gaXB0YWJsZXMgLXQgbmF0IC1BIFBPU1RST1VUSU5HIC1vIGV0aDAgLWogTUFTUVVFUkFERQ=="
     }
 SETTINGS
-}
+}*/
 
 
 
