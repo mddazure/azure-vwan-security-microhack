c05e3a73a66c230a11432f6bb418e1b58d0f6da9
Marc de Droog <madedroo@microsoft.com>
2023-07-07 22:14:22 +0200

    updates

--- templates/vnet-gw2.tf
+++ templates/vnet-gw2.tf
@@ -55,7 +55,14 @@ resource "azurerm_public_ip" "vnet-gw-onprem2-pubip-1" {
     bgp_settings {
       asn = 64000
       bgp_peering_address = azurerm_network_interface.nva-csr-vm-nic-2.private_ip_address
-    }
-
-    
+    }    
+  }
+  resource "azurerm_virtual_network_gateway_connection" "to-csr" {
+    name = "to-csr"
+    location = var.location-onprem2
+    resource_group_name = azurerm_resource_group.vwan-microhack-spoke-rg.name
+    type = "IPsec"
+    virtual_network_gateway_id = azurerm_virtual_network_gateway.vnet-gw-onprem2.id
+    local_network_gateway_id = azurerm_local_network_gateway.lng-csr.id
+    shared_key = "Microhack2023"    
   }
