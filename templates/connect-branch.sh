3b4b8bf0d706cfece1cae8c7ae1a4a85a6bdbac3
Marc de Droog <madedroo@microsoft.com>
2023-07-11 17:54:53 +0200

    updates

--- templates/connect-branch.sh
+++ templates/connect-branch.sh
@@ -1,15 +1,15 @@
 az extension add --name virtual-wan
 
 echo "# VNETGW: Get parameters from onprem vnet gateway"
-vnetgwtunnelip1=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-microhack-spoke-rg --query "bgpSettings.bgpPeeringAddresses[0].tunnelIpAddresses[0]" --output tsv)
+vnetgwtunnelip1=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-security-microhack-spoke-rg --query "bgpSettings.bgpPeeringAddresses[0].tunnelIpAddresses[0]" --output tsv)
 echo "VNET GW Tunnel address #1:" $vnetgwtunnelip1
-vnetgwtunnelip2=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-microhack-spoke-rg --query "bgpSettings.bgpPeeringAddresses[1].tunnelIpAddresses[0]" --output tsv)
+vnetgwtunnelip2=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-security-microhack-spoke-rg --query "bgpSettings.bgpPeeringAddresses[1].tunnelIpAddresses[0]" --output tsv)
 echo "VNET GW Tunnel address #2:" $vnetgwtunnelip2
-vnetgwbgpip1=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-microhack-spoke-rg --query "bgpSettings.bgpPeeringAddresses[0].defaultBgpIpAddresses"  --output tsv)
+vnetgwbgpip1=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-security-microhack-spoke-rg --query "bgpSettings.bgpPeeringAddresses[0].defaultBgpIpAddresses"  --output tsv)
 echo "VNET GW BGP address:" $vnetgwbgpip1
-vnetgwbgpip2=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-microhack-spoke-rg --query "bgpSettings.bgpPeeringAddresses[1].defaultBgpIpAddresses"  --output tsv)
+vnetgwbgpip2=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-security-microhack-spoke-rg --query "bgpSettings.bgpPeeringAddresses[1].defaultBgpIpAddresses"  --output tsv)
 echo "VNET GW BGP address:" $vnetgwbgpip2
-vnetgwasn=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-microhack-spoke-rg --query "bgpSettings.asn" --output tsv)
+vnetgwasn=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-security-microhack-spoke-rg --query "bgpSettings.asn" --output tsv)
 echo "VNET GW BGP ASN:" $vnetgwasn
 sharedkey="m1cr0hack"
 
@@ -28,8 +28,8 @@ hubgwasn=$(az network vpn-gateway show --name microhack-we-hub-vng  -g vwan-micr
 echo "Hub GW BGP ASN:" $hubgwasn
 
 echo "# create local network gateway"
-az network local-gateway create -g vwan-microhack-spoke-rg -n lng --gateway-ip-address $hubgwtunneladdress --location westeurope --asn $hubgwasn --bgp-peering-address $hubgwbgpaddress
+az network local-gateway create -g vwan-security-microhack-spoke-rg -n lng --gateway-ip-address $hubgwtunneladdress --location westeurope --asn $hubgwasn --bgp-peering-address $hubgwbgpaddress
 
 echo "# VNET GW: connect from vnet gw to local network gateway"
-az network vpn-connection create -n to-we-hub --vnet-gateway1 vnet-gw-onprem -g vwan-microhack-spoke-rg --local-gateway2 lng -l uksouth --shared-key $sharedkey --enable-bgp
+az network vpn-connection create -n to-we-hub --vnet-gateway1 vnet-gw-onprem -g vwan-security-microhack-spoke-rg --local-gateway2 lng -l uksouth --shared-key $sharedkey --enable-bgp
 
