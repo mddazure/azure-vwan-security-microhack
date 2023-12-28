01d4b95a3bf3fc411413043c948fdc380e72f57f
Marc de Droog <madedroo@microsoft.com>
2023-12-28 20:31:59 +0100

    addfws

--- templates/stop-fw.ps1
+++ templates/stop-fw.ps1
@@ -9,3 +9,16 @@ Set-AzFirewall -AzureFirewall $azfwwe
 $azfwuseast = Get-AzFirewall -Name "microhack-useast-hub-firewall" -ResourceGroupName "vwan-security-microhack-hub-rg"
 $azfwuseast.Deallocate()
 Set-AzFirewall -AzureFirewall $azfwuseast 
+
+# Stop swedencentral firewall
+
+$azfwsec = Get-AzFirewall -Name "AzureFirewall_microhack-sec-secured-hub" -ResourceGroupName "vwan-security-microhack-hub-rg"
+$azfwsec.Deallocate()
+Set-AzFirewall -AzureFirewall $azfwsec 
+
+
+# Stop germany west central firewall
+
+$azfwgewc = Get-AzFirewall -Name "AzureFirewall_microhack-gewc-secured-hub" -ResourceGroupName "vwan-security-microhack-hub-rg"
+$azfwgewc.Deallocate()
+Set-AzFirewall -AzureFirewall $azfwgewc 
